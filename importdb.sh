b6566037665462c2e114fdb5bab87d58aaa19154
root <root@labazor.mylabserver.com>
2019-07-08 09:43:53 +0000

    Remove ndn

--- importdb.sh
+++ importdb.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+#
 echo "Enter DB name: "
 read db
 echo "Enter DB you are importing name: "
--- killall.sh
+++ killall.sh
@@ -1,3 +1,4 @@
 #/bin/bash
+#1
 a=$(basename "$PWD")
 docker rm -f "$a"_web_1 "$a"_fpm7.0_1 "$a"_fpm5.6_1 "$a"_fpm7.2_1 "$a"_fpm7.1_1 "$a"_db_1 "$a"_redis_1
